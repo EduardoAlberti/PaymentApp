89a860dd3f1b27abf028c306a3334c50fce0540b
EduardoAlberti <eduardo.alberti.d@gmail.com>
2018-11-15 16:10:47 -0300

    Initial commit

---
+++ PaymentApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaymentApp
+//
+//  Created by Eduardo on 11/15/18.
+//  Copyright © 2018 EA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaymentApp/EAMainViewController.m
@@ -0,0 +1,23 @@
+//
+//  EAMainViewController.m
+//  PaymentApp
+//
+//  Created by Eduardo on 11/15/18.
+//  Copyright © 2018 EA. All rights reserved.
+//
+
+#import "EAMainViewController.h"
+
+@interface EAMainViewController ()
+
+@end
+
+@implementation EAMainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+@end
